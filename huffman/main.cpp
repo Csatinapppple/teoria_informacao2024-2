2095a828d5a39d35c7cc2595c4626da2acecf0cb
chplz <goodjob@nice.com>
2024-09-09 16:39:28 -0300

    a

---
+++ huffman/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+
+std::string encode(std::string input){
+	std::string ret;
+
+	std::unordered_map<char, int> occurrences;
+
+
+	return ret;
+}
+
+int main(){
+
+		std::string input = "aaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbdddddddcccc";
+
+		
+
+}
