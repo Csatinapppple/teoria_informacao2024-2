02a230d33f3e7c881942dd3e6aca39ddbdf5f773
chplz <goodjob@nice.com>
2024-09-10 09:59:45 -0300

    first ste

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -7,13 +7,21 @@ std::string encode(std::string input){
 	std::unordered_map<char, int> occurrences;
 
 
+	for (char c: input){
+		occurrences[c]=occurrences[c]+=1;
+	}
+
+	for (auto& elm: occurrences)
+		std::cout << elm.first << " " << elm.second << "\n";
+
+
 	return ret;
 }
 
 int main(){
 
-		std::string input = "aaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbdddddddcccc";
+		std::string input = "AABCBAD";
 
-		
+		std::string output = encode(input);
 
 }
