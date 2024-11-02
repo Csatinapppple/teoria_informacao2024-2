cb88a2c23b506aaa5eaab7c55267e41e45a66f66
cuds <ana@r.c>
2024-11-02 02:10:34 +0100

    encode

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -3,15 +3,18 @@
 #dividindo os bits em 2 partes
 
 def encode(char: str);
+    
 
 def encode_char(char: chr):
     first_half = ord(char) & 0b00001111
-    second_half = ord(char) >> 4 & 0b00001111
+    second_half = ord(char) >> 3 & 0b00001111
     
     print(bin(ord(char)))
 
     print(bin(first_half))
     print(bin(second_half))
+
+    print(
 
 
 encode('a')
