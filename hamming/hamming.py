4b9805f4f2ee4f5680f67772462a2986b23a8463
cuds <ana@r.c>
2024-11-02 01:37:15 +0100

    encode

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -2,10 +2,18 @@
 #hamming(7,4) encoder and decoder deve ser capaz de codificar simbolos char
 #dividindo os bits em 2 partes
 
-def encode(in: chr):
-    first_half = in & 0b00001111
-    second_half = in >> 4 & 0b00001111
+def encode(char: str);
 
+def encode_char(char: chr):
+    first_half = ord(char) & 0b00001111
+    second_half = ord(char) >> 4 & 0b00001111
     
+    print(bin(ord(char)))
+
+    print(bin(first_half))
+    print(bin(second_half))
+
+
+encode('a')
     
 
