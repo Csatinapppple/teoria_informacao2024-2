9de508cac37a49d14d7299bee7c53144ea91232b
a <a@a.c>
2024-11-11 02:23:54 +0100

    Added cifra

--- cifra/cifra.py
+++ cifra/cifra.py
@@ -10,10 +10,19 @@ def decifer(string):
         deciphered = ""
 
         for c in string:
+            
+            if c in alfabeto_value:
 
-            offset = (shift + alfabeto_value[c]) % len(alfabeto)
+                offset = (shift + alfabeto_value[c]) % len(alfabeto)
 
+                deciphered += alfabeto[offset]
+            else:
+                deciphered += c
+
+        print(deciphered, shift)
 
 
 print(alfabeto)
 print(alfabeto_value)
+
+decifer("slgqglmq")
