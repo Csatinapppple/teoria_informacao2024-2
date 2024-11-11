2355b0377ee408d17667d9437192e02b766539c6
a <a@a.c>
2024-11-11 02:10:31 +0100

    Added cifra

---
+++ cifra/cifra.py
@@ -0,0 +1,19 @@
+
+
+alfabeto = dict(enumerate("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@"))
+alfabeto_value = {v: k for k, v in alfabeto.items()}
+
+def decifer(string):
+    
+    for shift in range(0, len(alfabeto)):
+        
+        deciphered = ""
+
+        for c in string:
+
+            offset = shift + alfabeto
+
+
+
+print(alfabeto)
+print(alfabeto_value)
