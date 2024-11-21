a62f2c07cb7bc20abf5637664f26d55e87ac857e
cuds <ana@r.c>
2024-11-21 06:21:03 +0100

    adicionado main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import hamming.hamming
+import bsc.bsc
+
+
+first_input = """
+    Insira o que voce quer fazer:
+    1. Codificar
+    2. Decodificar
+"""
+
+second_input = """
+    Insira o algoritimo
+    1. BSC (Canal Binario Simetrico)
+    2. Hamming(7.4)
+"""
+
+third_input = """
+    Insira a entrada para o algoritimo:
+
+"""
+
+while(True):
+    
+    first = int(input(first_input))
+
+    while(True):
+
+        second
+    
+
